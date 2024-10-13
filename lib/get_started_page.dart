fac988ccbcc40272d9748187be19bbe81d165d5c
Mani <manibadboy66@gmail.com>
2024-10-13 22:38:11 +0530

    #

--- lib/get_started_page.dart
+++ lib/get_started_page.dart
@@ -6,38 +6,38 @@ class GetStartedPage extends StatefulWidget {
   @override
   State<GetStartedPage> createState() => _GetStartedPageState();
 }
- PageController _controller= PageController();
-class _GetStartedPageState extends State<GetStartedPage> {
 
-  final _currentPageIndex=0;
+PageController _controller = PageController();
+
+class _GetStartedPageState extends State<GetStartedPage> {
+  final _currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: PageView(
-          controller:_controller,
+          controller: _controller,
           physics: const BouncingScrollPhysics(),
-          onPageChanged: (int index){
-           setState(() {
-             _controller=index as PageController;
-           });
+          onPageChanged: (int index) {
+            setState(() {
+              _controller = index as PageController;
+            });
           },
           children: [
             Container(
               width: 200,
               height: 200,
               color: Colors.yellow,
-              child:const Center(
+              child: const Center(
                 child: Text('Slide 1'),
               ),
             ),
             Container(
               width: 200,
               height: 200,
-
               color: Colors.lightBlue,
-              child:const Center(
+              child: const Center(
                 child: Text('Slide 2'),
               ),
             ),
@@ -51,15 +51,16 @@ class _GetStartedPageState extends State<GetStartedPage> {
             )
           ],
         ),
-        floatingActionButton: FloatingActionButton(onPressed: (
-            ){
-          if(_currentPageIndex<3){
-            _controller.nextPage(duration: const Duration(milliseconds: 500), curve: Curves.ease);
-          }
-        },
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_currentPageIndex < 3) {
+              _controller.nextPage(
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.ease);
+            }
+          },
           child: const Icon(Icons.arrow_forward),
         ),
-
       ),
     );
   }
